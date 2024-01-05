38ce4510ff6425f8847d50126b9a4cc6f6b0ffcd
lblock181 <lblockfound@gmail.com>
2024-01-05 11:28:31 -0600

    fixed cors file. added extra todos in hanlders/mod & notes on req/resp

--- src/cors.rs
+++ src/cors.rs
@@ -1,26 +1,34 @@
-#[macro_use]
-extern crate rocket;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{ContentType, Header, Method, Status};
+use rocket::{Request, Response};
 
-mod cors;
-mod handlers;
-mod models;
+pub struct CORS;
 
-use cors::*;
-use handlers::*;
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
 
-#[launch]
-async fn rocket() -> _ {
-    rocket::build()
-        .mount(
-            "/",
-            routes![
-                create_question,
-                read_questions,
-                delete_question,
-                create_answer,
-                read_answers,
-                delete_answer
-            ],
-        )
-        .attach(CORS)
+    // source: https://stackoverflow.com/questions/62412361/how-to-set-up-cors-or-options-for-rocket-rs
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, DELETE, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+
+        // source: https://webprogramming.ninja/2022/08/25/handling-options-requests-in-rust-using-rocket-with-cors/
+        if request.method() == Method::Options {
+            let body = "";
+            response.set_header(ContentType::Plain);
+            response.set_sized_body(body.len(), std::io::Cursor::new(body));
+            response.set_status(Status::Ok);
+        }
+    }
 }
--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -8,23 +8,43 @@ use crate::models::*;
 pub async fn create_question(
     question: Json<Question>,
 ) -> Json<QuestionDetail> {
+    // request has title and descr
+    // return question_uuid, title, description, created_at (serialize question detail)
     todo!()
 }
 
 #[get("/questions")]
 pub async fn read_questions() -> Json<Vec<QuestionDetail>> {
+    // req -> no body
+    // serialize all questionDetails into vec
     todo!()
 }
 
 #[delete("/question", data = "<question_uuid>")]
-pub async fn delete_question(
-    question_uuid: Json<QuestionId>
-) {
+pub async fn delete_question(question_uuid: Json<QuestionId>) {
+    // req -> question_uuid
+    // response -> no body but 200 resp code
     todo!()
 }
 
 // ---- CRUD for Answers ----
 
+#[post("/answer", data = "<answer>")]
+pub async fn create_answer(
+    answer: Json<Answer>,
+) -> Json<AnswerDetail> {
+    todo!()
+}
+
+#[get("/answers")]
+pub async fn read_answers() -> Json<Vec<AnswerDetail>> {
+    todo!()
+}
+
+#[delete("/answer", data = "<answer_uuid>")]
+pub async fn delete_answer(answer_uuid: Json<AnswerId>) {
+    todo!()
+}
 // TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
 //       The handler function should be called `create_answer`.
 //       
